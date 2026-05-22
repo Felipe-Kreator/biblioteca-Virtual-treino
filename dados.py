b346603789f0b03075578d7f9c3bd58ea8b580fb
Felipe-Kreator <felipeestudante007@gmail.com>
2026-05-22 13:26:54 -0300

    Create dados.py

---
+++ dados.py
@@ -0,0 +1,8 @@
+lista_livros = []
+lista_autor = []
+lista_genero = []
+lista_ano = []
+lista_prioridade = []
+
+status_leitura = ('a ler', 'lendo', 'concluido')
+stats_priority = ('baixo', 'medio', 'alto')
